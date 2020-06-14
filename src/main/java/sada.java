350a9500bb0072bb07128ea13fe45d8e41596fb1
Mariia_Kryvokin <mkryvokin@corelogic.com>
2020-06-14 18:13:54 +0300

    hello world

---
+++ src/main/java/sada.java
@@ -0,0 +1,7 @@
+public class sada {
+    public static void main(String[] args) {
+        String cs ="d,lol, pp";
+        System.out.println(cs.indexOf("l"));
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="http://localhost:9090/squirrel.jpg">SQUIRREL</a>
+</body>
+</html>
